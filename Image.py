6ab9ab51ca7fcf730ae13e9fda6b9d72a4ca9437
Tony <109382836+peppacaiou@users.noreply.github.com>
2023-10-20 22:09:16 -0700

    created comments for image class

--- Image.py
+++ Image.py
@@ -1,8 +1,16 @@
 import PIL
 class Image:
-    """
-    
-    """
+    '''
+    A class to represent the Image 
+
+    Attribute image (PIL.Image.Image): The image that after being processed
+
+    Attribute size (int): The size, in bytes, of the image
+
+    Attribute length (int): The length (pixels) of the image
+
+    Attribute width (int): The width, (pixels), of the image
+    '''
     def __init__(self,imageLink="",size = 2048, length = 10,width =10):
         self.image:PIL.Image.Image =  PIL.Image.open(imageLink) 
         self.size:int = size
