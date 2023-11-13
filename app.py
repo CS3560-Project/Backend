4ced837ed76079bd8a4803b1f91b26a6a32d12fa
Tony <109382836+peppacaiou@users.noreply.github.com>
2023-11-13 12:28:48 -0800

    basic socket

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify,request
-
-
+from flask_socketio import SocketIO, emit
 from dotenv import load_dotenv
 import os
 from views.Account import Account
@@ -8,10 +7,12 @@ from views.Account import Account
 
 load_dotenv()
 app = Flask(__name__)
+socket = SocketIO(app)
 
 
-
-
+@socket.on('connect')
+def test_connect():
+    emit('after connect',  {'data':'Lets dance'})
 
 
 app.add_url_rule('/account/', view_func = Account.as_view('acc'))
