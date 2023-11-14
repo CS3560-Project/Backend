85774abd0d71676731d194339dd41c2d080b0476
CanaanMatsunaga <131640942+CanaanMatsunaga@users.noreply.github.com>
2023-11-14 11:46:08 -0800

    Create Channel.py

---
+++ views/Channel.py
@@ -0,0 +1,29 @@
+from flask import jsonify, request
+import json
+from utils.validator import validate_input
+from Exceptions.apiExceptions import *
+from flask.views import MethodView
+
+from Message import Message
+from database.channelDB import Channel  # Assuming you have a channel database module
+
+class ChannelView(MethodView):
+
+    def post(self):
+        # get the data from the request
+        required_fields = ["channelName"]
+        
+        data = json.loads(request.data.decode('utf-8'))
+        
+        try:
+            validate_input(data.keys(), required_fields)
+
+        except MissingArgumentException as e:
+            
+            return jsonify({"error": e.message}), e.error_code
+        
+        # create a new channel
+        channel = Channel(data["channelName"])
+        # You can save the channel to the database here if needed
+        
+        return jsonify({"success": "created", "channelID": channel.channelID}), 201
