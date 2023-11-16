f15449af534ab6bca67221ad677e81e4b1d1e59f
CanaanMatsunaga <131640942+CanaanMatsunaga@users.noreply.github.com>
2023-11-16 12:58:07 -0800

    Update Channel.py

--- views/Channel.py
+++ views/Channel.py
@@ -1,26 +1,53 @@
-from flask import jsonify,request
+from flask import jsonify, request, send_file
+import io
 import json
-from utils.validator import validate_input
-from Exceptions.apiExceptions import *
+import os
 from flask.views import MethodView
-
-from Message import Message
-from database.channelDB import Channel  # Assuming you have a channel database module
+from database.channelDB import Channel, Channeldb
+from utils.validator import validate_input
+from Exceptions.apiExceptions import MissingArgumentException
 
 class ChannelView(MethodView):
-
     def post(self):
-        # get the data from the request
         required_fields = ["channelName"]
-        
         data = json.loads(request.data.decode('utf-8'))
-        
+
         try:
             validate_input(data.keys(), required_fields)
 
         except MissingArgumentException as e:
-            
             return jsonify({"error": e.message}), e.error_code
-        
 
-        return jsonify({"success": "created", "channelID": channel.channelID}), 201
+        channel = Channel(channelName=data["channelName"])
+        channel_id = Channeldb.post_channel(channel)
+
+        return jsonify({"success": f"Channel '{data['channelName']}' created with ID {channel_id}"}), 201
+
+    def get(self):
+        channel_id = request.args.get("channelID")
+        
+        try:
+            validate_input([channel_id], ["channelID"])
+        except MissingArgumentException as e:
+            return jsonify({"error": e.message}), e.error_code
+
+        channel = Channeldb.get_channel(channel_id)
+
+        if channel:
+            return jsonify({"channelName": channel.get_channel_name, "channelID": channel.channelID})
+        else:
+            return jsonify({"error": "Channel not found"}), 404
+
+    def patch(self):
+        data = json.loads(request.data.decode('utf-8'))
+        required_fields = ["channelID", "newChannelName"]
+
+        try:
+            validate_input(data.keys(), required_fields)
+
+        except MissingArgumentException as e:
+            return jsonify({"error": e.message}), e.error_code
+
+        Channeldb.patch_channel_name(data["channelID"], data["newChannelName"])
+
+        return jsonify(f"Channel {data['channelID']} name changed to {data['newChannelName']}"), 200
