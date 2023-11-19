22e43559a38f801b2bb41663eb075ebf4e9c6e83
Jason <34526187+GuyWhoCode@users.noreply.github.com>
2023-11-19 15:22:12 -0800

    hotfix: removed files with same name

--- database/classServerDB.py
+++ database/classServerDB.py
@@ -1,5 +1,15 @@
 from .database import Database
 
-class ClassServer():
-    # implement crud operation here
-    pass
+class ClassServer:
+
+    @staticmethod
+    def createClassServer(serverName, serverProfilePicture=b''):
+        query = """
+            INSERT INTO ClassServer (serverName, serverProfilePicture)
+            VALUES (%s, %s);
+        """
+        values = (serverName, serverProfilePicture)
+
+        server_id = Database.query(query, values)
+
+        return server_id
