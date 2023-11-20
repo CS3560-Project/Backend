3d5bf62c88b2b114735c516d6a51f69d608e5db8
Tony <109382836+peppacaiou@users.noreply.github.com>
2023-11-20 10:41:13 -0800

    fixx for commit

--- database/classserverDB.py
+++ database/classserverDB.py
@@ -1,15 +1,5 @@
 from .database import Database
 
-class ClassServer:
-
-    @staticmethod
-    def createClassServer(serverName, serverProfilePicture=b''):
-        query = """
-            INSERT INTO ClassServer (serverName, serverProfilePicture)
-            VALUES (%s, %s);
-        """
-        values = (serverName, serverProfilePicture)
-
-        server_id = Database.query(query, values)
-
-        return server_id
+class ClassServer():
+    # implement crud operation here
+    pass
