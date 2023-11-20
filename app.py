1bffe7bc0ea698cdcfc3faca9520dd51d78fd283
Eloto04 <eloto8677@gmail.com>
2023-11-20 11:56:15 -0800

    Added CourseSection endpoints to app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 from views.Account import Account
 from views.Image import Image
+from views.CourseSection import CourseSection
 
 
 load_dotenv()
@@ -19,3 +20,4 @@ def test_connect():
 
 app.add_url_rule('/account/', view_func = Account.as_view('acc'))
 app.add_url_rule('/image/',view_func = Image.as_view('image'))
+app.add_url_rule('/section/',view_func = CourseSection.as_view('section'))
