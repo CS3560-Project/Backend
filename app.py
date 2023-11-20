71d33ce729df6a3372ea35b5d7467da36365ed96
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2023-11-20 11:53:45 -0800

    feat: allowed CORS

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import os
 from views.Account import Account
 from views.Image import Image
 from flask_cors import CORS
+import json
 
 
 load_dotenv()
 app = Flask(__name__)
-socket = SocketIO(app,logger=True, engineio_logger=True)
+socket = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
 
@@ -17,6 +18,10 @@ CORS(app)
 def test_connect():
     emit('after connect',  {'data':'Lets dance'})
 
+@socket.on('message')
+def handle_message(message):
+    print(message)
+    emit('message_received', json.dumps(message))
 
 app.add_url_rule('/account/', view_func = Account.as_view('acc'))
 app.add_url_rule('/image/',view_func = Image.as_view('image'))
