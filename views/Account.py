a24599838ed57e672216344841d85fc56bb12721
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2023-11-20 23:39:29 -0800

    feat: added default img server side

--- views/Account.py
+++ views/Account.py
@@ -1,4 +1,4 @@
-from flask import jsonify,request,make_response,send_file
+from flask import jsonify, request, make_response, send_file
 import io
 import json
 from utils.validator import validate_input
@@ -9,75 +9,69 @@ from database.userDB import User
 from database.imageDB import Image
 from database.imageDB import Image
 import os
-class Account(MethodView):
-    
-    
-    def post(self):
-        # get the data from 
-        
-        required_field = ["username","password","email"]
-        # data = request.files
-        data = json.loads(request.data.decode("utf-8"))
-        # print(data.get("file  "))
-        
 
-        
+class Account(MethodView):
+
+    def post(self):
+        required_field = ["username", "password", "email"]
+        data = json.loads(request.data.decode("utf-8"))
+
         try:
             validate_input(data.keys(), required_field)
 
         except MissingArgumentException as e:
-            
-            return jsonify({"error":e.message}),e.error_code
-        if "profilePicture" not in data.keys():
-            with open(os.path.join(os.getcwd(),"image","userProfiles","download.jpg"),"rb") as file:
-                image = file.read()
 
-                
-                
-                
+            return jsonify({"error": e.message}), e.error_code
+        if "profilePicture" not in data.keys():
+            with open(os.path.join(os.getcwd(), "image", "userProfiles", "download.jpg"), "rb") as file:
+                image = file.read()
+                image = "data:image/jpeg;base64," + base64.b64encode(image).decode('ASCII')
+                # Converts local image to base64 string 
+
         else:
             image = data["profilePicture"]
-            
-        
+
         imageID = Image.store_image(image)
 
-        userID = User.createAccount(data["username"],data["email"],data["password"],imageID )
-        
+        userID = User.createAccount(
+            data["username"], data["email"], data["password"], imageID)
 
-        return jsonify({"userID":userID}),201
+        return jsonify({"userID": userID}), 201
+
     def get(self):
         email = request.args.get("email")
         password = request.args.get("password")
         # implement a check on required arguments (when using validator function make sure is_body  is false to ensure correct error is sent)
-        
+
         db_val = User.getAccount(email=email)
-        if len(db_val) == 0 :
-            return jsonify({"error": f"no account associated with {email}"}),404
+        if len(db_val) == 0:
+            return jsonify({"error": f"no account associated with {email}"}), 404
         db_val = db_val[0]
         if password != db_val["password"]:
-            return jsonify({"error":"incorrect password"}),404
+            return jsonify({"error": "incorrect password"}), 404
         return jsonify({
-            "userID":db_val["userID"],
-            "userName":db_val["userName"],
-            "password":db_val["password"],
-            "imageID":db_val["profilePictureID"]
-        }),200
-        
+            "userID": db_val["userID"],
+            "userName": db_val["userName"],
+            "password": db_val["password"],
+            "imageID": db_val["profilePictureID"]
+        }), 200
+
     def patch(self):
         # changes the account
         data = json.loads(request.data.decode('utf-8'))
-        required_field = ["email","username","password"]
+        required_field = ["email", "username", "password"]
         try:
             validate_input(data.keys(), required_field)
 
         except MissingArgumentException as e:
-            
-            return jsonify({"error":e.message}),e.error_code
-        User.changeAccount(data.get("email"),data.get("username"),data.get("password"))
-        
-        return jsonify(f"{data.get('email')} changed"),200
-       
+
+            return jsonify({"error": e.message}), e.error_code
+        User.changeAccount(data.get("email"), data.get(
+            "username"), data.get("password"))
+
+        return jsonify(f"{data.get('email')} changed"), 200
+
     def delete(self):
-            email = request.args.get("email")
-            User.deteleteAccount(email=email)
-            return jsonify("account deleted")
+        email = request.args.get("email")
+        User.deteleteAccount(email=email)
+        return jsonify("account deleted")
--- views/Image.py
+++ views/Image.py
@@ -1,33 +1,20 @@
-from flask import jsonify,request,send_file
-import json
+from flask import jsonify, request
 from utils.validator import validate_input
 from Exceptions.apiExceptions import *
 from flask.views import MethodView
 from database import imageDB
-from io import BytesIO
 from PIL import Image as Img
+
 
 class Image(MethodView):
     def post(self):
         photo_byte = request.data
-        
-        
-        
         imageID = imageDB.Image.store_image(photo_byte)
-        return jsonify({"imageID":imageID}),201
+        return jsonify({"imageID": imageID}), 201
+
     def get(self):
-        # make error handler for thewse
         photoID = request.args.get("imageID")
         data = imageDB.Image.get_image(photoID)[0]
         image = data[1]
-        image_type = data[2]
-        
-        return send_file(
-            BytesIO(image),
-            mimetype = f"/{image_type}"
-        )
-        
 
-
-
-    
+        return image
