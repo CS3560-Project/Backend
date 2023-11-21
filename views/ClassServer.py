4846d144a4e80bb76c045e7e62688dd7baa3991a
CanaanMatsunaga <131640942+CanaanMatsunaga@users.noreply.github.com>
2023-11-20 21:47:05 -0800

    Update ClassServer.py

--- views/ClassServer.py
+++ views/ClassServer.py
@@ -1,33 +1,85 @@
 from flask import jsonify, request
+from flask.views import MethodView
 import json
 from utils.validator import validate_input
-from Exceptions.apiExceptions import *
-from flask.views import MethodView
-
-from database.ClassServer import ClassServer 
+from Exceptions.apiExceptions import MissingArgumentException
+from database.classServerDB import ClassServerDB
+from database.imageDB import Image
+from database.channelDB import ChannelThe as Channel
+import os
 
 class ClassServer(MethodView):
-
     def post(self):
-        # get the data from the request
+
         required_fields = ["serverName"]
-        
+
         data = json.loads(request.data.decode('utf-8'))
-        
+
         try:
             validate_input(data.keys(), required_fields)
 
         except MissingArgumentException as e:
-            
             return jsonify({"error": e.message}), e.error_code
+        print(data)
+        print("\n\n\n")
+   
+
+        if "imageID" not in data:
+            print("not in data")
+            print("\n\n\n")
+            with open(os.path.join(os.getcwd(),"image","userProfiles","download.jpg"),"rb") as file:
+                image = file.read()
+            
+        else:
+            image = data["imageID"]
+            pass
+    
         
-        # create a new ClassServer
-        server_name = data["serverName"]
-        server_profile_picture = b''
+        print("\n\n\n")
+        imageID = Image.store_image(image)
+        print(imageID)
+        
+        print("\n\n\n")
+        server_id = ClassServerDB.createClassServer(data["serverName"], imageID)
+        
+        
+        general_id = Channel.post_channel(channelName="general",serverID=server_id)
+        homework_id = Channel.post_channel(channelName="homework",serverID=server_id)
+        offtopic_id = Channel.post_channel(channelName="offtopic",serverID=server_id)
 
-        # Call the createClassServer method
-        server_id = ClassServer.createClassServer(server_name, server_profile_picture)
+
+
+        response_data = {
+            "success": "created",
+            "serverID": server_id,
+            "serverName": data["serverName"],
+            "defaultChannels": {
+                "general": Channel.get_channel(general_id),
+                "homework": Channel.get_channel(homework_id),
+                "offtopic": Channel.get_channel(offtopic_id)
+            }
+            }
         
 
+        return jsonify(response_data), 201
 
-        return jsonify({"success": "created", "serverID": server_id}), 201
+    def get(self):
+
+        # Assuming "serverID" is a required query parameter
+        server_id = request.args.get("serverID")
+
+        if not server_id:
+            return jsonify({"error": "Missing required parameter 'serverID'"}), 400
+
+        server = ClassServerDB.getClassServer(server_id)
+
+        if server:
+            all_channels = Channel.get_channel_from_server(server["serverID"])
+
+            return jsonify({
+                "serverName": server["serverName"],
+                "serverID": server["serverID"],
+                "channels": all_channels
+            })
+        else:
+            return jsonify({"error": "ClassServer not found"}), 404
