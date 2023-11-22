80fda2b0cbc607ecaee51f721c3643ea3abdb642
Tony <109382836+peppacaiou@users.noreply.github.com>
2023-11-21 17:56:51 -0800

    made barebones documentation

--- app.py
+++ app.py
@@ -22,15 +22,43 @@ CORS(app)
 
 
 
+"""
+post: takes ina username,password,email (and profilePicture is optional) and returns the userID
+get : takes in a email and password and returns userID,userName,password,imageID,serverIDs
+get: if instead only a userID is passed, username,passowrd,imageID and a list of serverIDs(not implemented yet)
 
+
+delete : takes an email and deletes it 
+"""
 app.add_url_rule('/account/', view_func = Account.as_view('acc'))
+
 app.add_url_rule('/image/',view_func = Image.as_view('image'))
 
-socketio.on_namespace(Message("/message"))
 
 
+"""
+post: takes in a  channelName and serverID and creates a channel in the specified server (channel ID is returned)
+get: takes ina  serverID and channelID and returns the channelName,channelID, serverID and serverName
+patch: takes in a channelID and a newChannelName and changes the name of the specified channel
+delete: takes ina  channelID and deletes it
+"""
 app.add_url_rule('/channel/',view_func = ChannelView.as_view('channel'))
+
+
+"""
+classServer
+post: take an server name and spits out a server with  3 channels predefined
+import thing to note is it returns the serverID which shoudl be stored
+get: takes a serverID and rerieves the serverNaame,ID all channels and all users associated wit it
+
+ """
 app.add_url_rule('/classserver/',view_func = ClassServer.as_view('classserver'))
+
+# use this get endpoint to get all messages associated witha  sepcifc channel oif a server
 app.add_url_rule('/message/',view_func = Message_endpoint.as_view('message'))
 
+# use this post endpoint to join users with servers
 app.add_url_rule('/userServer/',view_func = UserServer.as_view('userServer'))
+
+# important to note that userID,channelID and serverID cannot be null
+socketio.on_namespace(Message("/message"))
